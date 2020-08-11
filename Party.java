dd43f84ebd7685b5c18e7c8728de90aa61d2bc51
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-08-11 10:05:53 +0530

    Add files via upload

---
+++ Party.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+class Party{
+    public static void main(String[ ] args) {
+      Frame f = new Frame();
+      Label l = new Label("Party Some Where");
+      Button b = new Button("You Say");
+      Button c = new Button("Shoot");
+      Panel p = new Panel();
+      p.add(l);
+      System.out.println("What");
+
+    }//comments
+
+} /*multi
+line
+comments*/
+class Text{
+  public static void main(String[ ] args){
+    String str = "Hello, there!";
+    System.out.println(str);
+  }
+
+
+}
