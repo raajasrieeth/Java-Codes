56cc4da3763955ef4ee6ed8d987161f7c254caf9
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-08-15 18:15:08 +0530

    Add files via upload

---
+++ some.java
@@ -0,0 +1,11 @@
+import java.awt.*
+
+public class some{
+    public static void main(String[] args){
+        String name = "Raaj";
+        String hello = "Hello there";
+        int age = 14;
+        System.out.print(hello + " " + name + " you are " + age + " years old.");
+    }
+
+}
