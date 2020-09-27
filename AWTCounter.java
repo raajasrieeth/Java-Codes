76ca60774acbee4b47ad7cc7b77336c9c32f8241
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-09-27 18:20:06 +0530

    Update AWTCounter.java

--- AWTCounter.java
+++ AWTCounter.java
@@ -1,3 +1,6 @@
+//make a counter displaying the number of clicks.
+//Gui with java
+
 import java.awt.*;        // Using AWT container and component classes
 import java.awt.event.*;  // Using AWT event classes and listener interfaces
  
